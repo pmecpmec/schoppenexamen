9d2fad90652b971342bbf6793bfa0a4055f409b8
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-19 14:41:32 +0100

    Update

--- admin.php
+++ admin.php
@@ -4,31 +4,74 @@
 <link rel="stylesheet" href="style.css">
 </header>
 <body>
+
 <div class="Admin">
   <form action="/form/submit" method="GET"> 
   <input type="text" name="text" class="search" placeholder="Search For">
 </form>
 </div>
-<div class="Admin-h1">
-<p>Accounts</p>
-</div>
-</div>
+
+ <div class="Admin-h1">
+ <p>Accounts</p>
+ </div>
+
 <div class="Accounts">
-  <button>Button</button>
-  <button>Button</button>
-</div>
+  <table>
+    <tr>
+      <th>Users</th>
+    </tr>
+    <tr>
+      <td>Lorem Ipsum</td>
+    </tr>
+  </table>
+  </div>
+
+   <div class="Button-Accounts">
+    <button>Button</button>
+    <button>Button</button>
+   </div>
+
 <div class="Admin-h2">
 <p>Vacatures</p>
 </div>
+
 <div class="Vacature">
-  <button>Button</button>
-  <button>Button</button>
-</div>
+  <table>
+    <tr>
+      <th>Vacatures</th>
+      <th>Companies</th>
+    </tr>
+    <tr>
+      <td>Lorem Ipsum</td>
+      <td>Lorem Ipsum</td>
+    </tr>
+  </table>
+  </div>
+
+  <div class="Button-Vacature">
+    <button>Button</button>
+    <button>Button</button>
+  </div>
+
 <div class="Admin-h3">
 <p>CV</p>
 </div>
+
 <div class="CV">
-  <button>Button</button>
-  <button>Button</button>
-</div>
+  <table>
+    <tr>
+      <th>CV</th>
+      <th>Users</th>
+    </tr>
+    <tr>
+      <td>Lorem Ipsum</td>
+      <td>Lorem Ipsum</td>
+    </tr> 
+  </table>
+  </div>
+
+  <div class="Button-CV">
+    <button>Button</button>
+    <button>Button</button>
+  </div>
 </body>
