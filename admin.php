b6ca02f9e303a783f1264afa061ca6be6df9834e
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-19 12:14:59 +0100

    Admin

--- admin.php
+++ admin.php
@@ -9,17 +9,24 @@
   <input type="text" name="text" class="search" placeholder="Search For">
 </form>
 </div>
-<h1>Accounts</h1>
+<div class="Admin-h1">
+<p>Accounts</p>
+</div>
+</div>
 <div class="Accounts">
   <button>Button</button>
   <button>Button</button>
 </div>
-<h2>Vacatures</h2>
+<div class="Admin-h2">
+<p>Vacatures</p>
+</div>
 <div class="Vacature">
   <button>Button</button>
   <button>Button</button>
 </div>
-<h3>CV</h3>
+<div class="Admin-h3">
+<p>CV</p>
+</div>
 <div class="CV">
   <button>Button</button>
   <button>Button</button>
