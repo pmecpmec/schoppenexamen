fb114ff0ddb1746fe2ccae92ae9a878ab82177d4
pmecpmec <pmecmail@gmail.com>
2022-12-19 13:38:55 +0100

    Authentication update

--- authenticate.php
+++ authenticate.php
@@ -1,13 +1,47 @@
 <?php
 session_start();
-// Change this to your connection info.
-$DATABASE_HOST = 'localhost';
+$DATABASE_HOST = '20.160.76.183';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'mysql1234';
 $DATABASE_NAME = 'phplogin';
-// Try and connect using the info above.
+// Probeert een connectie te maken met de database.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
-  // If there is an error with the connection, stop the script and display the error.
+  // Als er een foutnis krijg je een fout melding
   exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+} // Kijkt nu of de informatie klopt wat is ingevuld.
+if (!isset($_POST['username'], $_POST['password'])) {
+  // Heeft de informatie niet binnengekregen.
+  exit('Vul alstublieft uw gebruikersnaam en wacthwoord in!');
+}
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+  $stmt->bind_param('s', $_POST['username']);
+  $stmt->execute();
+  // Bewaar de informatie in de database als het account bestaat.
+  $stmt->store_result();
+
+
+  $stmt->close();
+}
+$stmt->store_result();
+if ($stmt->num_rows > 0) {
+  $stmt->bind_result($id, $password);
+  $stmt->fetch();
+  // Account bestaat en nu wordt het wachtwoord gecontroleerd.
+  //
+  if (password_verify($_POST['password'], $password)) {
+    // Het is gelukt u heeft ingelogd.
+    // Crëer een sessie en stuur de gebruiker door naar de welkom pagina.
+    session_regenerate_id();
+    $_SESSION['loggedin'] = TRUE;
+    $_SESSION['name'] = $_POST['username'];
+    $_SESSION['id'] = $id;
+    echo 'Welkom ' . $_SESSION['name'] . '!';
+  } else {
+    // Verkeerde Wacthwoord
+    echo 'Onjuiste gebruikersnaam en/of wachtwoord!';
+  }
+} else {
+  // Vekeerde Gebruikersnaam
+  echo 'Onjuiste gebruikersnaam en/of wachtwoord!';
 }
