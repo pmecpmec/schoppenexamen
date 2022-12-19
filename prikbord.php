d617906637ea8c0fbd16892dee25482b6a656c25
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-19 11:44:47 +0100

    Prikbord

---
+++ prikbord.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<header>
+   <link rel="stylesheet" href="style.css">
+</header>
+<body>
+<div class="Prikbord">
+  <form action="/form/submit" method="GET"> 
+  <input type="text" name="text" class="search" placeholder="#">
+  <input type="submit" name="submit" class="submit" value="#">
+</form>
+</div>
+</body>
+</html>
