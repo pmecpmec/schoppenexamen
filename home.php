b0329f33f10b05ee172ca519d308f1155c2e687c
Thomas Verhage <thomasverhage@outlook.com>
2022-12-19 14:10:32 +0100

    home edit

--- home.php
+++ home.php
@@ -5,12 +5,20 @@
 <center>
     <h1>Home</h1>
 </center>
+
+<ul>
+  <li><a href="#---">file</a></li>
+  <li><a href="#---">account</a></li>
+  <li><a href="#---">login</a></li>
+</ul>
+
 </head>
 
 <body>
-    <center>
+<center>
     <p>Welcome</p>
-    <center>
+</center>
+
 </body>
 
 </html>
