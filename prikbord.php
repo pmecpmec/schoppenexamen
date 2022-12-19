98f23312c111739f14a3bf6f5fd033f608d661de
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-19 11:47:09 +0100

    Zoek bar

--- prikbord.php
+++ prikbord.php
@@ -7,7 +7,6 @@
 <div class="Prikbord">
   <form action="/form/submit" method="GET"> 
   <input type="text" name="text" class="search" placeholder="#">
-  <input type="submit" name="submit" class="submit" value="#">
 </form>
 </div>
 </body>
