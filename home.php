e462322025e80dfd92343c2afc3cf1c7a2d943ce
pmecpmec <pmecmail@gmail.com>
2022-12-20 10:17:28 +0100

    home.php

--- home.php
+++ home.php
@@ -5,33 +5,3 @@ if (!isset($_SESSION['loggedin'])) {
   header('Location: index.html');
   exit;
 }
-?>
-<!DOCTYPE html>
-<html>
-
-<head>
-  <meta charset="utf-8">
-  <title>Home Page</title>
-  <link href="style.css" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-  <?php
-include 'header.php'
-?>
-</head>
-
-<body class="loggedin">
-  <nav class="navtop">
-    <div>
-      <h1>Website Title</h1>
-      <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-      <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-    </div>
-  </nav>
-  <div class="homecontent">
-    <h2>Home Page</h2>
-    <p>Welcome back, <?= $_SESSION['name'] ?>!</p>
-  </div>
-  <footer id="footer"><?php include'footer.php' ?></footer>
-</body>
-
-</html>
