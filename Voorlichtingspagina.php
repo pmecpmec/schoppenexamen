1e746d29ef2d9ee9fcaab7adfd51354dd3b237e8
Thomas Verhage <thomasverhage@outlook.com>
2022-12-21 13:40:25 +0100

    voorlichtingspagina update

--- Voorlichtingspagina.php
+++ Voorlichtingspagina.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
+
 <html>
-    <header>
+    
+
+<header>                                                                                                                                        <!--head-->
+
+<center>
         <h1>Voorlichtingspagina</h1>
+</center>
+
 </header>
-<body>
+
+<body>                                                                                                                                              <!--body-->
+
+<center>   
+<p>Deze website is van Internationaal uitzendbureau XXL </p>                                                                                    <!--Toelichtings tekst-->
+<p>Op deze wensite is een multimedia-prikbord aanwezig waar werkzoekenden en bedrijven uit verschillende landen zich kunnen aanmelden.</p>
+</center>
+
 </body>
 
 </html>
