95df97dea1220dfb511a82e0083b8c107ffab94a
pmecpmec <pmecmail@gmail.com>
2022-12-22 09:54:21 +0100

    auth

--- register.php
+++ register.php
@@ -20,9 +20,9 @@ if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['emai
   // One or more values are empty.
   exit('Please complete the registration form');
 }
-// E-mail Validatie
+// E-mail Validatie 
 if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-	exit('Email is not valid!');
+  exit('Email is not valid!');
 }
 // Verkeerde Karakters Validatie
 if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
@@ -30,7 +30,7 @@ if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
 }
 // Karakter Lengte
 if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
-	exit('Password must be between 5 and 20 characters long!');
+  exit('Password must be between 5 and 20 characters long!');
 }
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
   // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
@@ -48,7 +48,14 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
       $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
       $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
       $stmt->execute();
-      echo 'You have successfully registered, you can now login!';
+      $from    = 'noreply@yourdomain.com';
+      $subject = 'Account Activation Required';
+      $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+      // Update the activation variable below
+      $activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $_POST['email'] . '&code=' . $uniqid;
+      $message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+      mail($_POST['email'], $subject, $message, $headers);
+      echo 'Please check your email to activate your account!';
     } else {
       // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
       echo 'Could not prepare statement!';
