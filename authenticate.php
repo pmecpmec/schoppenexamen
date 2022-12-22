bdefbd56f7d9aca55785237592cca987e4ff56ab
pmecpmec <pmecmail@gmail.com>
2022-12-22 11:35:54 +0100

    auth

--- authenticate.php
+++ authenticate.php
@@ -2,7 +2,7 @@
 session_start();
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'mysql1234';
 $DATABASE_NAME = 'phplogin';
 // Probeert een connectie te maken met de database.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
