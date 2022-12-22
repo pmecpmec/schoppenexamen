2050a3ab6e77defe42471d3017a98c83707d7731
pmecpmec <pmecmail@gmail.com>
2022-12-22 13:40:57 +0100

    header

--- header.php
+++ header.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <header>
-    <link rel="stylesheet" href="style.css">   
+  <link rel="stylesheet" href="style.css">
 </header>
+
 <body>
-    <div class="header">
-        <img class="headerlogoimg" src="xxl_logo.png">
-        <div class="language"">
+  <div class="header">
+    <a href="http://20.160.155.255/index.php"> <img class="headerlogoimg" src="xxl_logo.png">
+      <div class="language"">
             <button>Taal</button>
+        </a>
         </div>
-        <div class="file"">
-            <button><a href="prikbord.php">Bestanden</a> </button>
-        </div>
-        <div class="account"">
+        <div class=" file"">
+        <button><a href="prikbord.php">Bestanden</a> </button>
+      </div>
+      <div class="account"">
             <button>Account</button>
         </div>
 
-        <div class="loginandout"">
-            <button>Login and out</button>
-        </div>
-    </div>
+        <div class=" loginandout"">
+        <button>Login and out</button>
+      </div>
+  </div>
 </body>
+
 </html>
