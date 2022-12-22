535f43fc747bcbea20d46bf564387cb4a1038175
pmecpmec <pmecmail@gmail.com>
2022-12-22 12:21:16 +0100

    yes

--- authenticate.php
+++ authenticate.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-$DATABASE_HOST = '0.0.0.0';
+$DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = 'mysql1234';
+$DATABASE_PASS = '';
 $DATABASE_NAME = 'schoppenexamen';
 // Probeert een connectie te maken met de database.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
