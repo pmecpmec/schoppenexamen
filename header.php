82488b02f2c5e563ed98e96331bc1d0cf2495d74
pmecpmec <pmecmail@gmail.com>
2022-12-22 14:42:44 +0100

    header

--- header.php
+++ header.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <header>
-  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
 </header>
 
 <body>
     <div class="header">
-    <a href="http://20.160.155.255/index.php">
-      <img class="headerlogoimg" src="xxl_logo.png">
-        <!--Toolbar-->
+        <a href="http://20.160.155.255/index.php">
+            <img class="headerlogoimg" src="xxl_logo.png">
+            <!--Toolbar
       <ul>
         <li><a href="#">Taal</a></li>
         <li><a href="#">Bestanden</a></li>
         <li><a href="#">Account</a></li>
         <li><a href="#">Login and out</a></li>
       </ul>
-    </div>
+  </div>
 </body>
 
 </html>
