4654997848c5d530d1d5cbdfe2bb921fb198852e
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-22 14:35:39 +0100

    header

--- header.php
+++ header.php
@@ -5,13 +5,14 @@
 </header>
 <body>
     <div class="header">
-      <img class="headerlogoimg" src="xxl_logo.png" href="index.php">
+        <a href="http://20.160.155.255/index.php">
+      <img class="headerlogoimg" src="xxl_logo.png">
         <!--Toolbar-->
       <ul>
-        <li><a href="#">Taal</a></li>
-        <li><a href="#">Bestanden</a></li>
-        <li><a href="#">Account</a></li>
-        <li><a href="#">Login and out</a></li>
+        <li><a href="index.html">Homepage</a></li>
+        <li><a href="information.html">Information</a></li>
+        <li><a href="extra.html">Extra</a></li>
+        <li><a href="signup.html">Sign Up</a></li>
       </ul>
     </div>
 </body>
