1624a34c1eb55d9a654e02792744575510f9aa25
pmecpmec <pmecmail@gmail.com>
2022-12-22 11:43:21 +0100

    update

--- authenticate.php
+++ authenticate.php
@@ -3,7 +3,7 @@ session_start();
 $DATABASE_HOST = '0.0.0.0';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = 'mysql1234';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'schoppenexamen';
 // Probeert een connectie te maken met de database.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
--- priveregister.php
+++ priveregister.php
@@ -3,7 +3,7 @@
 $DATABASE_HOST = '0.0.0.0';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = 'mysql1234';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'schoppenexamen';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
