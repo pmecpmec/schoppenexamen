41e6e142e13300a612e595cf8a29240c89a4d29a
Qualiteit <51707651+Qualiteit@users.noreply.github.com>
2022-12-22 10:28:05 +0100

    prikbord

--- authenticate.php
+++ authenticate.php
@@ -2,7 +2,7 @@
 session_start();
 $DATABASE_HOST = '0.0.0.0';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = 'mysql1234';
+$DATABASE_PASS = '';
 $DATABASE_NAME = 'phplogin';
 // Probeert een connectie te maken met de database.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
