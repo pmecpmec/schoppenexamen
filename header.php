845346a10f6c6a820fe3b717e53443a75835057b
aa254365 <113343284+aa254365@users.noreply.github.com>
2022-12-22 15:13:22 +0100

    header

--- header.php
+++ header.php
@@ -3,18 +3,23 @@
 <header>
   <link rel="stylesheet" href="style.css">
 </header>
-
 <body>
-  <div class="header">
+    <div class="header">
     <a href="http://20.160.155.255/index.php">
       <img class="headerlogoimg" src="xxl_logo.png">
+        <!--Toolbar-->
       <ul>
-        <li><a href="20.160.155.255/register.php">Registreren</a></li>
-        <li><a href="20.160.155.255/login.php">Login</a></li>
+        <li class="taal">
+          <a href="javascript:void(0)" class="talen">Talen</a>
+          <div class="Talen-Content">
+            <p>Nederlands</p>
+            <p>English</p>
+          </div>
         <li><a href="20.160.155.255/prikbord.php">Bestanden</a></li>
-        <li><a href="20.160.155.255/profile.php">Account</a></li <li><a href="#">Taal</a></li>
+        <li><a href="20.160.155.255/profile.php">Account</a></li>
+        <li><a href="20.160.155.255/login.php">Login and out</a></li>
+        <li><a href="20.160.155.255/register.php">Registreren</a></li>
       </ul>
-  </div>
+    </div>
 </body>
-
 </html>
