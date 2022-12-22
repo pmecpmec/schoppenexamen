33e511635c71e1440617cc29cd16255f5ae545b1
pmecpmec <pmecmail@gmail.com>
2022-12-22 13:30:02 +0100

    voorlichting

---
+++ voorlichting.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<?php
+include 'header.php'
+?>
+<html>
+
+
+<head>
+  <!--head-->
+
+  <meta charset="utf-8">
+  <title>Home</title>
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+
+  <center>
+    <h1>Voorlichtingspagina</h1>
+  </center>
+
+</head>
+
+<br>
+</br>
+
+<body>
+  <!--body-->
+
+  <center>
+    <p>Deze website is van internationaal uitzendbureau XXL.</p>
+    <!--Toelichtings tekst-->
+    <p>Op deze wensite is een multimedia-prikbord aanwezig waar werkzoekenden en bedrijven uit verschillende landen zich
+      kunnen aanmelden.</p>
+    <p>Hier kan informatie opgenomen worden over zaken zoals werk zoeken, arbeidsovereenkomsten, arbeidsvoorwaarden,
+      contracten, ethiek.</p>
+  </center>
+
+  <br>
+  </br>
+
+  <footer id="footer"><?php include 'footer.php' ?>
+    <!--footer-->
+
+    <center>
+      <p>@ XXL 2022 1-800-243-0000</p>
+    </center>
+
+  </footer>
+
+</body>
+
+</html>
